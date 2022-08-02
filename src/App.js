b06aa9cb9f0eae454edf3d6e079446ceb0437d8e
Hernan Salti <hernansalti@gmail.com>
2022-08-02 17:14:49 -0300

    V2.0 desafio_4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar'
-import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
+import Navbar from './Components/Navbar/Navbar';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
+import Counter from './Components/Counter/Counter';
 
 function App() {
+
+  const handleOnAdd = (quantity) => {
+    console.log(`la cantidad agregada es: ${quantity}`)
+  }
   return (
     <div className="App">
       <div>
         <h1 class='titulo'>Tienda Nonno Bazar</h1>
       </div>
       <Navbar />
-      <ItemListContainer greeting='Bienvenidos'/> 
+      <ItemListContainer greeting='Bienvenidos'/>
+      <Counter stock={10} onAdd={handleOnAdd}/>
 
     </div>
     
--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,35 @@
 import './ItemListContainer.css'
+import { useState, useEffect } from 'react'
+import { getProductsNonno } from '../../asyncMock'
 
-const ItemListContainer = ({greeting}) => {
+import ItemList from '../ItemList/ItemList'
+
+const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        getProductsNonno().then(products => {
+            setProducts(products)
+        })
+    }, [])
+    // ***hasta aca quede ver como separar los contadores por separado y ver como hay que entregar***
+    // const productsComp = products.map(prod => <li key={prod.id}>{prod.name}</li>)
+    return (
+        <>
+            <h1>{greeting}</h1>
+            {/* <ul> */}
+                {/* {productsComp} */}
+                {/* {products.map(prod => <li key={prod.id}>{prod.name}</li>)}
+            </ul>         */}
+            <ItemList products={products}/>
+        </>
+
+    )
+}
+/*/const ItemListContainer = ({greeting}) => {
     return (
         <h1 class='titulo' >{greeting}</h1>
     )
-}
+}*/
 
 export default ItemListContainer
