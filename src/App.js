01f8827bc1c3b5f7c4d54111a052371c3bdcf82e
Hernan Salti <hernansalti@gmail.com>
2022-08-22 23:26:57 -0300

    v9 y 10

--- src/App.js
+++ src/App.js
@@ -1,28 +1,3 @@
-// import './App.css';
-// import Navbar from './Components/Navbar/Navbar.js';
-// import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
-// import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
-// function App() {
-
-//   return (
-//     <div className="App"> 
-//       <BrowserRouter>
-//         <Navbar />
-//         <Routes>
-//           <Route path='/' element={<ItemListContainer greeting='Productos destacados'/>}/>
-//           <Route path='/category/:categoryId' element={<ItemListContainer greeting='Listado filtrado'/>} />
-//           <Route path='/detail/:productId' element={<ItemDetailContainer />} />  
-//           <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
-//         </Routes>
-//       </BrowserRouter>
-      
-//     </div>
-//   );
-// }
-
-// export default App;
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
